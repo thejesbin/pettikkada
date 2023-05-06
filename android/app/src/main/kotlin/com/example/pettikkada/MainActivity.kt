619b692112cc1e6538ce9e91a32f53f58fcd5cde
Jesbin <thejesbin@gmail.com>
2023-05-06 20:24:33 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/pettikkada/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pettikkada
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/constants.dart
@@ -0,0 +1,9 @@
+const appName="PettiKKada";
+
+
+
+
+//test images
+const watchImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaIEFJ8orHf9nT5mlOr_oKjSBPpSICRwQnKlVkAm0y1sEjwFzHYh-6eY0KIuHVEHz0imU&usqp=CAU";
+const phoneImgae="https://rukminim1.flixcart.com/image/416/416/xif0q/mobile/6/x/j/-original-imaghxejqvpwfqh2.jpeg?q=70";
+const budsImage="https://m.media-amazon.com/images/I/61aasAbKvvL._SL1500_.jpg";
